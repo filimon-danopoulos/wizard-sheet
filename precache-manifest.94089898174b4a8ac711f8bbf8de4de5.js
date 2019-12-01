734dadd75de6af262fac7c71a58e1e65dc6b5d53
Filimon Danopoulos <filimon.danopoulos@gmail.com>
2019-12-01 22:24:05 +0100

    Updates

---
+++ precache-manifest.94089898174b4a8ac711f8bbf8de4de5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d77d9b6a4b85c0195c7",
+    "url": "/wizard-sheet/css/app.3f60366f.css"
+  },
+  {
+    "revision": "1075cdd97a2aeb854e28",
+    "url": "/wizard-sheet/css/chunk-vendors.1905ca2d.css"
+  },
+  {
+    "revision": "06b1f866078d0254288f05a18f52fb14",
+    "url": "/wizard-sheet/index.html"
+  },
+  {
+    "revision": "0d77d9b6a4b85c0195c7",
+    "url": "/wizard-sheet/js/app.ea5fcbb8.js"
+  },
+  {
+    "revision": "1075cdd97a2aeb854e28",
+    "url": "/wizard-sheet/js/chunk-vendors.e8cfa885.js"
+  },
+  {
+    "revision": "fe72fba2ef9da82fd6120833ac7ea776",
+    "url": "/wizard-sheet/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wizard-sheet/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wizard-sheet/precache-manifest.c6246f8cb912b76f0ca00ba95cdcae9e.js"
+  "/wizard-sheet/precache-manifest.94089898174b4a8ac711f8bbf8de4de5.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "wizard-sheet"});
